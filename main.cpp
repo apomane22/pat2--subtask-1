33480be704a973a0c63dd0a4c9f237e8f53ad4e2
apomane22 <thatomapomane44@gmail.com>
2026-05-11 13:11:37 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string message;
+    string morseCodes[26] = {
+        ".-", "-...", "-.-.","-..", ".", "..-.", "--.", "...." "..", ".---", //from A to J
+        "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", //from K to T
+        "..-", "...-", ".--", "-..-", "-.--", "--.." //from U to Z
+    };
+
+    cout << "Enter a massage in English (A-Z characters only): ";
+    getline(cin, message);
+
+    string fullMorseCode = "";
+
+    cout << "\nOutput Morse code:" << endl;
+
+    for (int i = 0; i < message.length(); i++){
+        char ch = toupper(message[i]);
+
+        if (ch >= 'A' && ch <= 'Z') {
+            string morse = morseCodes[ch - 'A'];
+
+            for (int j = 0; j < morse.length(); j++){
+                if (morse[j] == '.'){
+                    cout << char(46);
+                } else if (morse[j] == '-'){
+                    cout << char(45);
+                }
+            }
+            cout << " ";
+            fullMorseCode += morse + " ";
+
+            cout << endl << ch << ": ";
+            for (int j = 0; j < morse.length(); j++){
+                if (morse[j] == '.'){
+                    cout << char(46);
+                } else if (morse[j] == '-'){
+                    cout << char(45);
+                }
+            }
+            cout << endl;
+        }
+        else if (ch == ' '){
+            fullMorseCode += " ";
+        }
+
+    }
+    cout << "\nFull Morse code with spaces:" << endl;
+    cout << fullMorseCode << endl;
+
+
+
+
+
+    return 0;
+}
